8f8d2b0b89144ead14d278ea411e70c393ae5cf9
PolyakovSergey <ancientcult89@gmail.com>
2022-07-21 21:23:21 +0700

    page 78

---
+++ PartyInvites/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PartyInvites.Models;
+using System.Diagnostics;
+
+namespace PartyInvites.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult RsvpForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult RsvpForm(GuestResponse guestResponse)
+        {
+            Repository.AddResponse(guestResponse);
+            return View("Thanks", guestResponse);
+        }
+
+        public IActionResult ListResponses()
+        {
+            return View(Repository.Responses.Where(r => r.WillAttend == true));
+        }
+    }
+}
---
+++ PartyInvites/Models/Repository.cs
@@ -0,0 +1,13 @@
+﻿namespace PartyInvites.Models
+{
+    public static class Repository
+    {
+        private static List<GuestResponse> _responses = new List<GuestResponse>();
+        public static List<GuestResponse> Responses => _responses;
+        public static void AddResponse(GuestResponse response)
+        {
+            Console.WriteLine(response);
+            Responses.Add(response);
+        }
+    }
+}
