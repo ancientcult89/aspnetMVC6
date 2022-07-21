2a51dda8a46d41b4efc80958e4a72abb52fdb4b6
PolyakovSergey <ancientcult89@gmail.com>
2022-07-21 20:40:19 +0700

    page 64

---
+++ FirstProject/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using FirstProject.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace FirstProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            int hour = DateTime.Now.Hour;
+            string viewModel = hour < 12 ? "Good morning" : "Good afternoon";
+            return View("MyView", viewModel);
+        }
+
+    }
+}
