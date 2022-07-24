63c7c8e3caa8c9e9c3f62cf2072bef640fa2ae93
PolyakovSergey <ancientcult89@gmail.com>
2022-07-24 16:13:10 +0700

    page 193

--- SportsStore/SportsStore/Controllers/HomeController.cs
+++ SportsStore/SportsStore/Controllers/HomeController.cs
@@ -1,9 +1,18 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SportsStore.Models;
 
 namespace SportsStore.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index() => View();
+        private IStoreRepository _repository;
+        public int pageSize = 3;
+
+        public HomeController(IStoreRepository repository)
+        { 
+            _repository = repository;
+        }
+        public IActionResult Index(int productPage = 1) => View(_repository.Products.OrderBy(p => 
+            p.ProductId).Skip((productPage - 1) * pageSize).Take(pageSize));
     }
 }
---
+++ SportsStore/SportsStore/Models/PageInfo.cs
@@ -0,0 +1,11 @@
+﻿namespace SportsStore.Models
+{
+    public class PageInfo
+    {
+        public int TotalItems { get; set; }
+        public int ItensPerPage { get; set; }
+        public int CurrentPage { get; set; }
+
+        public int TotalPage => (int)Math.Ceiling((decimal)TotalItems / TotalItems);
+    }
+}
