328d45cc6a150eed11caf2071f942532e3910787
PolyakovSergey <ancientcult89@gmail.com>
2022-07-24 13:17:42 +0700

    page 171

--- Testing/SimpleApp.Tests/HomeControllerTests.cs
+++ Testing/SimpleApp.Tests/HomeControllerTests.cs
@@ -3,16 +3,17 @@ using System.Collections.Generic;
 using SimpleApp.Controllers;
 using SimpleApp.Models;
 using Xunit;
+using Moq;
 
 namespace SimpleApp.Tests
 {
     public class HomeControllerTests
     {
-        class FakeDataSource : IDataSource 
-        {
-            public FakeDataSource(Product[] data) => Products = data;
-            public IEnumerable<Product> Products { get; set; }
-        }
+        //class FakeDataSource : IDataSource 
+        //{
+        //    public FakeDataSource(Product[] data) => Products = data;
+        //    public IEnumerable<Product> Products { get; set; }
+        //}
 
         [Fact]
         public void IndexActionModelIsComplete()
@@ -24,16 +25,19 @@ namespace SimpleApp.Tests
                 new Product { Name = "P2", Price = 120M },
                 new Product { Name = "P3", Price = 110M }
             };
-            IDataSource data = new FakeDataSource(testData);
+            var mock = new Mock<IDataSource>();
+            mock.SetupGet(x => x.Products).Returns(testData);
+            //IDataSource data = new FakeDataSource(testData);
             var controller = new HomeController();
-            controller.dataSource = data;
+            controller.dataSource = mock.Object;
 
             //act
             var model = (controller.Index() as ViewResult)?.ViewData.Model as IEnumerable<Product>;
 
             //assert
-            Assert.Equal(data.Products, model, Comparer.Get<Product>(
+            Assert.Equal(testData, model, Comparer.Get<Product>(
                 (p1, p2) => p1?.Name == p2?.Name && p1?.Price == p2?.Price));
+            mock.VerifyGet(x => x.Products, Times.Once);
         }
     }
 }
