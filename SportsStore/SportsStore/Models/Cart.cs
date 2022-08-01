8e69937a97e88076b4df8b890e98c7939eb70fb7
PolyakovSergey <ancientcult89@gmail.com>
2022-08-01 21:59:33 +0700

    page 256

--- SportsStore/SportsStore/Models/Cart.cs
+++ SportsStore/SportsStore/Models/Cart.cs
@@ -3,7 +3,7 @@
     public class Cart
     {
         public List<CartLine> Lines { get; set; } = new List<CartLine>();
-        public void AddItem(Product product, int quantity)
+        public virtual void AddItem(Product product, int quantity)
         { 
             CartLine? line = Lines.Where(p => p.Product.ProductId == product.ProductId).FirstOrDefault();
 
@@ -12,11 +12,11 @@
             else line.Quantity += quantity;
         }
 
-        public void RemoveLine(Product product) => Lines.RemoveAll(l => l.Product.ProductId == product.ProductId);
+        public virtual void RemoveLine(Product product) => Lines.RemoveAll(l => l.Product.ProductId == product.ProductId);
 
         public decimal ComputeTotalValue() => Lines.Sum(e => e.Product.Price * e.Quantity);
 
-        public void Clear() => Lines.Clear();
+        public virtual void Clear() => Lines.Clear();
 
     }
 
--- SportsStore/SportsStore/Pages/Cart.cshtml.cs
+++ SportsStore/SportsStore/Pages/Cart.cshtml.cs
@@ -8,17 +8,18 @@ namespace SportsStore.Pages
     public class CartModel : PageModel
     {
         private IStoreRepository _repository;
-        public CartModel(IStoreRepository repository)
+        public CartModel(IStoreRepository repository, Cart cartServive)
         { 
             _repository = repository;
+            Cart = cartServive;
         }
 
-        public Cart? Cart { get; set; }
+        public Cart Cart { get; set; }
         public string ReturnUrl { get; set; } = "/";
         public void OnGet(string returnUrl)
         {
             ReturnUrl = returnUrl ?? "/";
-            Cart = HttpContext.Session.GetJson<Cart>("cart") ?? new Cart();
+            //Cart = HttpContext.Session.GetJson<Cart>("cart") ?? new Cart();
         }
 
         public IActionResult OnPost(long productId, string returnUrl)
@@ -26,10 +27,14 @@ namespace SportsStore.Pages
             Product? product = _repository.Products.FirstOrDefault(p => p.ProductId == productId);
             if (product != null)
             {
-                Cart = HttpContext.Session.GetJson<Cart>("cart") ?? new Cart();
                 Cart.AddItem(product, 1);
-                HttpContext.Session.SetJson("cart", Cart);
             }
+            return RedirectToPage(new { returnUrl = returnUrl });
+        }
+
+        public IActionResult OnPostRemove(long productId, string returnUrl)
+        {
+            Cart.RemoveLine(Cart.Lines.First(cl => cl.Product.ProductId == productId).Product);
             return RedirectToPage(new { returnUrl = returnUrl });
         }
     }
