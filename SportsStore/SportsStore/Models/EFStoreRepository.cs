83570354cde4e4fe8a6a6af189b0ec4ce3f27c25
PolyakovSergey <ancientcult89@gmail.com>
2022-08-04 21:05:02 +0700

    page 287

--- SportsStore/SportsStore/Models/EFStoreRepository.cs
+++ SportsStore/SportsStore/Models/EFStoreRepository.cs
@@ -8,5 +8,22 @@
             _dbContext = storeDbContext;
         }
         public IQueryable<Product> Products => _dbContext.Products;
+
+        public void CreateProduct(Product p)
+        {
+            _dbContext.Add(p);
+            _dbContext.SaveChanges();
+        }
+
+        public void DeleteProduct(Product p)
+        {
+            _dbContext.Remove(p);
+            _dbContext.SaveChanges();
+        }
+
+        public void SaveProduct(Product p)
+        {
+            _dbContext.SaveChanges();
+        }
     }
 }
