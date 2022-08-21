6e1c12f551d230a2d1ca25a4728e6ff949f29985
PolyakovSergey <ancientcult89@gmail.com>
2022-08-21 08:36:58 +0700

    page 378

--- Platform/Platform/Program.cs
+++ Platform/Platform/Program.cs
@@ -4,10 +4,27 @@ using Platform;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.Configure<MessageOption>(options => { options.CityName = "Albany"; });
+builder.Services.Configure<RouteOptions>(opts => { opts.ConstraintMap.Add("countryName", typeof(CountryRouteConstraint)); });
 
 var app = builder.Build();
 
-app.MapGet("files/{filename}.{ext}", async context => {
+app.Use(async (context, next) => { 
+    Endpoint? end = context.GetEndpoint();
+    if (end != null)
+    {
+        await context.Response.WriteAsync($"{end.DisplayName} Selected\n");
+    }
+    else
+        await context.Response.WriteAsync("No endpoint selected\n");
+    await next();
+});
+
+app.Map("{number:int}", async context => { await context.Response.WriteAsync("Routed to the int endpoint"); })
+    .WithDisplayName("Int endpoint").Add(b => ((RouteEndpointBuilder)b).Order = 1);
+app.Map("{number:double}", async context => { await context.Response.WriteAsync("Routed to the double endpoint"); })
+    .WithDisplayName("Double endpoint").Add(b => ((RouteEndpointBuilder)b).Order = 2);
+
+app.MapGet("{first:alpha:length(3)}/{second:bool}", async context => {
     await context.Response.WriteAsync("Request was Routed\n");
     foreach (var kvp in context.Request.RouteValues)
     {
@@ -15,7 +32,8 @@ app.MapGet("files/{filename}.{ext}", async context => {
     }
 });
 
-app.MapGet("capital/{country}", Capital.Endpoint);
-app.MapGet("size/{city}", Population.Endpoint).WithMetadata(new RouteNameMetadata("population"));
+app.MapGet("capital/{country:countryName}", Capital.Endpoint);
+app.MapGet("size/{city?}", Population.Endpoint);
+app.MapFallback(async context => { await context.Response.WriteAsync("Routed to fallback endpoint"); });
 
 app.Run();
