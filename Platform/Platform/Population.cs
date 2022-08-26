0ce01f56c2df80b10e1f7c1aa23e33ac60f23175
PolyakovSergey <ancientcult89@gmail.com>
2022-08-26 20:41:16 +0700

    page 443

--- Platform/Platform/Population.cs
+++ Platform/Platform/Population.cs
@@ -1,9 +1,11 @@
 ﻿namespace Platform
 {
-    public class Population
+    public partial class Population
     {
-        public static async Task Endpoint(HttpContext context)
+        public static async Task Endpoint(HttpContext context, ILogger<Population> logger)
         {
+            //logger.LogDebug($"Started processing for {context.Request.Path}");
+            StartingResponse(logger, context.Request.Path);
             string city = context.Request.RouteValues["city"] as string ?? "london";
             int? pop = null;
             switch (city.ToLower())
@@ -24,7 +26,13 @@
                 return;
             }
             else
+            { 
                 context.Response.StatusCode = StatusCodes.Status400BadRequest;
+            }
+            logger.LogDebug($"Finished processing for {context.Request.Path}");
         }
+
+        [LoggerMessage(0, LogLevel.Debug, "Starting response for {path}")]
+        public static partial void StartingResponse(ILogger logger, string path);
     }
 }
--- Platform/Platform/Program.cs
+++ Platform/Platform/Program.cs
@@ -1,30 +1,20 @@
 using Platform;
-//using Platform.Services;
+using Microsoft.AspNetCore.HttpLogging;
 
 var builder = WebApplication.CreateBuilder(args);
 
-var servicesConfig = builder.Configuration;
-builder.Services.Configure<MessageOption>(servicesConfig.GetSection("Location"));
-
-var serviceEnv = builder.Environment;
+builder.Services.AddHttpLogging(opts => {
+    opts.LoggingFields = HttpLoggingFields.RequestMethod | HttpLoggingFields.RequestPath | HttpLoggingFields.ResponseStatusCode;
+});
 
 var app = builder.Build();
 
-var pipelineConfig = app.Configuration;
+app.UseHttpLogging();
+//var logger = app.Services.GetService<ILoggerFactory>().CreateLogger("Pipeline");
 
-var piplineEnv = app.Environment;
-
-app.UseMiddleware<LocationMiddleware>();
-
-app.MapGet("config", async (HttpContext context, IConfiguration config, IWebHostEnvironment env) => {
-    string defaultDebug = config["Logging:Loglevel:Default"];
-    await context.Response.WriteAsync($"The config setting is: {defaultDebug}");
-    await context.Response.WriteAsync($"\nThe env setting is: {env.EnvironmentName}");
-    string wsID = config["WebService:Id"];
-    string wsKey = config["WebService:Key"];
-    await context.Response.WriteAsync($"\nThe secret ID is: {wsID}");
-    await context.Response.WriteAsync($"\nThe secret Key is: {wsKey}");
-});
+//logger.LogDebug("Pipeline configuration starting");
+app.MapGet("population/{city?}", Population.Endpoint);
+//logger.LogDebug("Pipeline configuration complete");
 
 app.MapGet("/", async context => {
     await context.Response.WriteAsync("Hello world!");
