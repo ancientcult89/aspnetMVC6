492c34f0d6910a26c0415d4e1b8a7771ac85a842
PolyakovSergey <ancientcult89@gmail.com>
2022-08-28 08:51:25 +0700

    page 469

---
+++ Platform/Platform/ConsentMiddleware.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http.Features;
+
+namespace Platform
+{
+    public class ConsentMiddleware
+    {
+        private RequestDelegate _next;
+
+        public ConsentMiddleware(RequestDelegate nextDelegate)
+        { 
+            _next = nextDelegate;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            if (context.Request.Path == "/consent")
+            {
+                ITrackingConsentFeature? consentFeature = context.Features.Get<ITrackingConsentFeature>();
+                if (consentFeature != null)
+                {
+                    if (!consentFeature.HasConsent)
+                        consentFeature.GrantConsent();
+                    else
+                        consentFeature.WithdrawConsent();
+                    await context.Response.WriteAsync(consentFeature.HasConsent ? "Consent Granted \n" : "Consent Withdrawn\n");
+                }
+            }
+            else
+                await _next(context);
+        }
+    }
+}
--- Platform/Platform/Program.cs
+++ Platform/Platform/Program.cs
@@ -1,30 +1,54 @@
 using Platform;
-using Microsoft.AspNetCore.HttpLogging;
-using Microsoft.Extensions.FileProviders;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddHttpLogging(opts => {
-    opts.LoggingFields = HttpLoggingFields.RequestMethod | HttpLoggingFields.RequestPath | HttpLoggingFields.ResponseStatusCode;
+builder.Services.AddDistributedMemoryCache();
+builder.Services.AddSession(opts => { 
+    opts.IdleTimeout = TimeSpan.FromMinutes(30);
+    opts.Cookie.IsEssential = true;
+});
+
+builder.Services.AddHsts(opts => { 
+    opts.MaxAge = TimeSpan.FromDays(1);
+    opts.IncludeSubDomains = true;
+});
+
+builder.Services.Configure<CookiePolicyOptions>(opts => {
+    opts.CheckConsentNeeded = context => true;
+});
+builder.Services.AddHttpsRedirection(opts => {
+    opts.RedirectStatusCode = StatusCodes.Status307TemporaryRedirect;
+    opts.HttpsPort = 443;
 });
 
 var app = builder.Build();
+if(app.Environment.IsProduction())
+    app.UseHsts();
 
-app.UseHttpLogging();
-//var logger = app.Services.GetService<ILoggerFactory>().CreateLogger("Pipeline");
+app.UseHttpsRedirection();
+app.UseSession();
 
-app.UseStaticFiles();
+app.UseCookiePolicy();
+app.UseMiddleware<ConsentMiddleware>();
 
-var env = app.Environment;
-app.UseStaticFiles(new StaticFileOptions { FileProvider = new PhysicalFileProvider($"{env.ContentRootPath}/staticfiles"),
-    RequestPath = "/files"
+app.MapGet("/session", async context => {
+    int counter1 = (context.Session.GetInt32("counter1") ?? 0) + 1;
+    int counter2 = (context.Session.GetInt32("counter2") ?? 0) + 1;
+    context.Session.SetInt32("counter1", counter1);
+    context.Session.SetInt32("counter2", counter2);
+    await context.Session.CommitAsync();
+    await context.Response.WriteAsync($"Counter1: {counter1}, Counter2: {counter2}");
 });
 
-//logger.LogDebug("Pipeline configuration starting");
-app.MapGet("population/{city?}", Population.Endpoint);
-//logger.LogDebug("Pipeline configuration complete");
+app.MapGet("clear", context => {
+    context.Response.Cookies.Delete("counter1");
+    context.Response.Cookies.Delete("counter2");
+    context.Response.Redirect("/");
+    return Task.CompletedTask;
+});
 
-app.MapGet("/", async context => {
+app.MapFallback(async context => {
+    await context.Response.WriteAsync($"HTTPS Request: {context.Request.IsHttps}\n");
     await context.Response.WriteAsync("Hello world!");
 });
 
