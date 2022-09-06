77d4d4d21fb514e973a6a18526fba520c79f8d8e
PolyakovSergey <ancientcult89@gmail.com>
2022-09-06 20:10:13 +0700

    page 524

--- WebApp/WebApp/Program.cs
+++ WebApp/WebApp/Program.cs
@@ -12,42 +12,11 @@ builder.Services.AddDbContext<DataContext>(opts => {
     opts.EnableSensitiveDataLogging(true);
 });
 
+builder.Services.AddControllers();
+
 var app = builder.Build();
 
-const string _baseurl = "/api/products";
-
-app.MapGet($"{_baseurl}/{{id}}", async (HttpContext context, DataContext data) => {
-    
-    string? id = context.Request.RouteValues["id"] as string;
-    if (id != null)
-    {
-        Product? p = data.Products.Find(long.Parse(id));
-        if (p == null)
-            context.Response.StatusCode = StatusCodes.Status404NotFound;
-        else
-        { 
-            context.Response.ContentType = "application/json";
-            await context.Response.WriteAsync(JsonSerializer.Serialize<Product>(p));
-        }
-    }
-});
-
-app.MapGet(_baseurl, async (HttpContext context, DataContext data) => { 
-    context.Response.ContentType="application/json";
-    await context.Response.WriteAsync(JsonSerializer.Serialize<IEnumerable<Product>>(data.Products));
-});
-
-app.MapPost(_baseurl, async (HttpContext context, DataContext data) => {
-    Product? p = await JsonSerializer.DeserializeAsync<Product>(context.Request.Body);
-    if (p != null)
-    {
-        await data.AddAsync(p);
-        await data.SaveChangesAsync();
-        context.Response.StatusCode = StatusCodes.Status200OK;
-    }
-});
-
-app.UseMiddleware<WebApp.TestMiddleware>();
+app.MapControllers();
 
 app.MapGet("/", () => "Hello World!");
 
