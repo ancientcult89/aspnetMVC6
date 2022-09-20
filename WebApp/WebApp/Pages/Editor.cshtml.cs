cc6ee946a6db8e2777dbec84970bf6c2a26493be
PolyakovSergey <ancientcult89@gmail.com>
2022-09-20 22:01:15 +0700

    page 672

---
+++ WebApp/WebApp/Pages/Editor.cshtml.cs
@@ -0,0 +1,30 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using WebApp.Models;
+
+namespace WebApp.Pages
+{
+    public class EditorModel : PageModel
+    {
+        private DataContext _dataContext;
+        public Product? Product { get; set; }
+        public EditorModel(DataContext ctx)
+        { 
+            _dataContext = ctx;
+        }
+
+        public async Task OnGetAsync(long id = 1)
+        { 
+            Product = await _dataContext.Products.FindAsync(id);
+        }
+
+        public async Task<IActionResult> OnPostAsync(long id, decimal price)
+        { 
+            Product? p = await _dataContext.Products.FindAsync(id);
+            if (p != null)
+                p.Price = price;
+            await _dataContext.SaveChangesAsync();
+            return RedirectToPage();
+        }
+    }
+}
--- WebApp/WebApp/Pages/Index.cshtml.cs
+++ WebApp/WebApp/Pages/Index.cshtml.cs
@@ -1,12 +1,24 @@
-using Microsoft.AspNetCore.Mvc;
+using WebApp.Models;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc;
 
 namespace WebApp.Pages
 {
     public class IndexModel : PageModel
     {
-        public void OnGet()
+        private DataContext _dataContext;
+        public Product? Product { get; set; }
+        public IndexModel(DataContext dataContext)
         {
+            _dataContext = dataContext;
+        }
+  
+        public async Task<IActionResult> OnGetAsync(long id = 1)
+        {
+            Product = await _dataContext.Products.FindAsync(id);
+            if(Product == null)
+                return RedirectToPage("NotFound");
+            return Page();
         }
     }
 }
