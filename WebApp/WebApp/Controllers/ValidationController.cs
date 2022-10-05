c27bfd64f770c8a95d00adc8ee409fd5a66ef30b
PolyakovSergey <ancientcult89@gmail.com>
2022-10-05 21:55:51 +0700

    page 910

---
+++ WebApp/WebApp/Controllers/ValidationController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApp.Models;
+
+namespace WebApp.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ValidationController : ControllerBase
+    {
+        private DataContext _dataContext;
+
+        public ValidationController(DataContext dataContext)
+        { 
+            _dataContext = dataContext;
+        }
+        [HttpGet("categorykey")]
+        public bool CategoryKey(string? categoryId, [FromQuery] KeyTarget target)
+        {
+            long keyVal;
+            return long.TryParse(categoryId ?? target.CategoryId, out keyVal) && _dataContext.Categories.Find(keyVal) != null;
+        }
+
+        [HttpGet("supplierkey")]
+        public bool SuplierKey(string? supplierId, [FromQuery] KeyTarget target)
+        {
+            long keyVal;
+            return long.TryParse(supplierId ?? target.SupplierId, out keyVal) && _dataContext.Suppliers.Find(keyVal) != null;
+        }
+
+        [Bind(Prefix = "Product")]
+        public class KeyTarget
+        { 
+            public string CategoryId { get; set; }
+            public string SupplierId { get; set; }
+        }
+    }
+}
--- WebApp/WebApp/Models/Product.cs
+++ WebApp/WebApp/Models/Product.cs
@@ -2,22 +2,31 @@
 using System.Text.Json.Serialization;
 using System.ComponentModel.DataAnnotations;
 using Microsoft.AspNetCore.Mvc.ModelBinding;
+using WebApp.Validation;
+using Microsoft.AspNetCore.Mvc;
 
 namespace WebApp.Models
 {
+    [PhraseAndPrice(Phrase = "Small", Price = "100")]
     public class Product
     {
         public long ProductId { get; set; }
+        [Required(ErrorMessage = "Please enter a value")]
         public string Name { get; set; } = string.Empty;
 
+        [Range(1, 999999, ErrorMessage = "Please enter a positive price")]
         [Column(TypeName = "decimal(8,2)")]
         //[DisplayFormat(DataFormatString = "{0:c2}", ApplyFormatInEditMode = true)]
-        [BindNever]
+        //[BindNever]
         public decimal Price { get; set; }
 
+        [PrimaryKey(ContextType = typeof(DataContext), DataType = typeof(Category))]
+        [Remote("CategoryKey", "Validation", ErrorMessage = "Enter an existing key")]
         public long CategoryId { get; set; }
         public Category? Category { get; set; }
 
+        [PrimaryKey(ContextType = typeof(DataContext), DataType = typeof(Supplier))]
+        [Remote("SupplierKey", "Validation", ErrorMessage = "Enter an existing key")]
         public long SupplierId { get; set; }
 
         [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
