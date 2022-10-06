dfa1b61f0bbf77f760e4890c63c36cd31849c540
PolyakovSergey <ancientcult89@gmail.com>
2022-10-06 20:59:40 +0700

    page 915

--- WebApp/WebApp/Controllers/HomeController.cs
+++ WebApp/WebApp/Controllers/HomeController.cs
@@ -6,24 +6,12 @@ namespace WebApp.Controllers
 {
     public class HomeController : Controller
     {
-        private DataContext context;
-        public HomeController(DataContext ctx)
+        public IActionResult Index()
         {
-            context = ctx;
-        }
-        public async Task<IActionResult> Index(long id = 1)
-        {
-            ViewBag.AveragePrice = await context.Products.AverageAsync(p => p.Price);
-            return View(await context.Products.FindAsync(id));
-        }
-        public IActionResult List()
-        {
-            return View(context.Products);
-        }
-
-        public IActionResult Html()
-        { 
-            return View((object)"This is a <h3><i>string</i></h3>");
+            if (Request.IsHttps)
+                return View("Message", "This is the Index action on the Home controller");
+            else
+                return new StatusCodeResult(StatusCodes.Status403Forbidden);
         }
     }
 }
--- WebApp/WebApp/Program.cs
+++ WebApp/WebApp/Program.cs
@@ -1,54 +1,25 @@
 using Microsoft.EntityFrameworkCore;
 using WebApp.Models;
-using Microsoft.AspNetCore.Antiforgery;
-//using Microsoft.AspNetCore.Razor.TagHelpers;
-//using WebApp.TagHelpers;
-using Microsoft.AspNetCore.Mvc;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<DataContext>(opts => {
-    opts.UseSqlServer(builder.Configuration["ConnectionStrings:ProductConnection"]);
+    opts.UseSqlServer(builder.Configuration[
+    "ConnectionStrings:ProductConnection"]);
     opts.EnableSensitiveDataLogging(true);
 });
-
 builder.Services.AddControllersWithViews();
-
 builder.Services.AddRazorPages();
-builder.Services.AddSingleton<CitiesData>();
-builder.Services.Configure<AntiforgeryOptions>(opts => {
-    opts.HeaderName = "X-XSRF-TOKEN";
-});
-//builder.Services.AddTransient<ITagHelperComponent, TimeTagHelperComponent>();
-//builder.Services.AddTransient<ITagHelperComponent, TableFooterTagHelperComponent>();
-
-builder.Services.Configure<MvcOptions>(opts 
-    => opts.ModelBindingMessageProvider.SetValueMustNotBeNullAccessor(value => "Please enter a value"));
 
 var app = builder.Build();
 
 app.UseStaticFiles();
-//app.MapControllers();
-//app.MapDefaultControllerRoute();
-
-IAntiforgery antiforgery = app.Services.GetRequiredService<IAntiforgery>();
-app.Use(async (context, next) => {
-    if (!context.Request.Path.StartsWithSegments("/api"))
-    { 
-        string? token = antiforgery.GetAndStoreTokens(context).RequestToken;
-        if (token != null)
-        {
-            context.Response.Cookies.Append("X-XSRF-TOKEN", token, new CookieOptions { HttpOnly = false });
-        }
-    }
-    await next();
-});
-
-app.MapControllerRoute("forms", "controllers/{controller=Home}/{action=Index}/{id?}");
+app.MapDefaultControllerRoute();
 app.MapRazorPages();
 
 var context = app.Services.CreateScope().ServiceProvider
     .GetRequiredService<DataContext>();
-    SeedData.SeedDatabase(context);
+
+SeedData.SeedDatabase(context);
 
 app.Run();
