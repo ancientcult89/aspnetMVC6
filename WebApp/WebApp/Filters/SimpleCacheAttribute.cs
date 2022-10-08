b31b8c9ec7d137662822730b4e6778f5335a613f
PolyakovSergey <ancientcult89@gmail.com>
2022-10-08 09:54:10 +0700

    page 958

---
+++ WebApp/WebApp/Filters/SimpleCacheAttribute.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace WebApp.Filters
+{
+    public class SimpleCacheAttribute : Attribute, IAsyncResourceFilter
+    {
+        private Dictionary<PathString, IActionResult> CachedResponses = new Dictionary<PathString, IActionResult>();
+
+        public async Task OnResourceExecutionAsync(ResourceExecutingContext context, ResourceExecutionDelegate next)
+        {
+            PathString path = context.HttpContext.Request.Path;
+            if (CachedResponses.ContainsKey(path))
+            {
+                context.Result = CachedResponses[path];
+                CachedResponses.Remove(path);
+            }
+            else
+            {
+                ResourceExecutedContext executedContext = await next();
+                if(executedContext.Result != null)
+                    CachedResponses.Add(context.HttpContext.Request.Path, executedContext.Result);
+            }
+        }
+    }
+}
