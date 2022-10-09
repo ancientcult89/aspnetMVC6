de3e16f9f33cf4040a325ac205870a686cbacf5b
PolyakovSergey <ancientcult89@gmail.com>
2022-10-09 14:23:18 +0700

    page 988

---
+++ WebApp/WebApp/Pages/EditorPageModel.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using WebApp.Models;
+
+namespace WebApp.Pages
+{
+    public class EditorPageModel : PageModel
+    {
+        public DataContext DataContext { get; set; }
+        public EditorPageModel(DataContext dataContext) {
+            DataContext = dataContext;
+        }
+        public IEnumerable<Category> Categories => DataContext.Categories;
+        public IEnumerable<Supplier> Suppliers => DataContext.Suppliers;
+
+        public ProductViewModel? ViewModel { get; set; }
+
+        protected async Task CheckNewCategory(Product product)
+        {
+            if (product.CategoryId == -1 && !string.IsNullOrEmpty(product.Category?.Name))
+            {
+                DataContext.Categories.Add(product.Category);
+                await DataContext.SaveChangesAsync();
+                product.CategoryId = product.Category.CategoryId;
+                ModelState.Clear();
+                TryValidateModel(product);
+            }
+        }
+    }
+}
