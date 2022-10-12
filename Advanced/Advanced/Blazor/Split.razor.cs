1c3431910b0b00d6f0f8a500ef12e80361a5b227
PolyakovSergey <ancientcult89@gmail.com>
2022-10-12 19:34:53 +0700

    page 1041

---
+++ Advanced/Advanced/Blazor/Split.razor.cs
@@ -0,0 +1,14 @@
+﻿using Advanced.Models;
+using Microsoft.AspNetCore.Components;
+
+namespace Advanced.Blazor
+{
+    public partial class Split
+    {
+        [Inject]
+        public DataContext? DataContext { get; set; }
+
+        public IEnumerable<string> Names => 
+            DataContext?.People.Select(p => p.Firstname) ?? Enumerable.Empty<string>();
+    }
+}
